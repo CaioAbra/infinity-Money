69bb1a27cc5649c3dde50e7fe988078529ae1ea0
CaioAbra <caioabra41@gmail.com>
2024-10-23 01:11:23 -0300

    pagina de sinais

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -10,7 +10,36 @@ $('#sidebarClose').on('click', function () {
     $('#sidebarToggle').find('i').removeClass('fa-times').addClass('fa-th-large');  // Volta o ícone de toggle
 
     $('#sidebarToggle').css('opacity', '1');
-    $('.navbar-brand').css('opacity', '1'); 
+    $('.navbar-brand').css('opacity', '1');
 });
 
 
+// Função para alternar entre as fases com temporizadores
+function iniciarFases() {
+    // Exibir fase 1
+    $('#fase1').show();
+    setTimeout(function () {
+        $('#fase1').hide(); // Esconde fase 1
+        $('#fase2').show(); // Exibe fase 2
+    }, 3000); // Fica 3 segundos na fase 1
+
+    setTimeout(function () {
+        $('#fase2').hide(); // Esconde fase 2
+        $('#fase3').show(); // Exibe fase 3
+    }, 6000); // 3 segundos depois da fase 2
+
+    setTimeout(function () {
+        $('#fase3').hide(); // Esconde fase 3
+        $('#fase4').show(); // Exibe fase 4
+    }, 9000); // 3 segundos depois da fase 3
+}
+
+// Iniciar fases assim que a página carregar
+$(document).ready(function () {
+    iniciarFases();
+});
+
+// Exibe o gráfico quando "Abrir Atlas" for clicado
+$('#abrirAtlas').on('click', function () {
+    $('#fase4').show(); // Exibe a fase 4
+});
